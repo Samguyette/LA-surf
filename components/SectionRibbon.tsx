41ebe95f51a656ff2db572b535ccb4c7fef0799a
Sam Guyette <samguyette@gmail.com>
2025-08-12 20:09:41 -0700

    Update SectionRibbon.tsx

--- components/SectionRibbon.tsx
+++ components/SectionRibbon.tsx
@@ -120,6 +120,7 @@ export default function SectionRibbon({ waveData, mapCenter }: SectionRibbonProp
                 </div>
               </div>
             )
+          })}
         </div>
       </div>
     </div>
